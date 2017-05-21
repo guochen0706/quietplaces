0f0305a6a4b6554d3a8a6aa0f2375445c1f9f1a5
cmathias <chrismathiaster@gmail.com>
2017-05-21 11:50:18 -0500

    Plugging api response into controller

--- ui/main.py
+++ ui/main.py
@@ -21,16 +21,28 @@ def version():
 def getNoiseScores():
     return getLodgingsForLatLong(lat=30.268162,long=-97.7417)
 
-def getLodgingsForLatLong(lat, lng):
-    createReponseObjectForResults(qpd.geo_search(lat=lat, lng=lng))
+def getLodgingsForLatLong(lat, lng, range=1):
+    output = []
+    for lodging_result in qpd.geo_search(lat=lat, lng=lng, range=range):
+        lodging = {}
+        lodging["lat"] = lodging_result["lat"]
+        lodging["long"] = lodging_result["lng"]
+        lodging["score"] = lodging_result["score"]
+        lodging["name"] = lodging_result["name"]
+        lodging["fs_id"] = lodging_result["lat"]
+        lodging["places_id"] = lodging_result["google_id"]
+        lodging["google_reviews"] = lodging_result["google_reviews"]
+        if "reviews" in lodging_result: lodging["fs_reviews"] = lodging_result["reviews"]
 
-def createReponseObjectForResults(results):
-    return '{"hotels": [{"lat": 30.268162,"long": -97.7417,"score": 20,"name": "Driskill Hotel","id": 1,"heatmap": []}, {"lat": 30.264444,"long": -97.74184900000002,"score": 60,"name": "Hyatt Place Downtown","id": 2,"heatmap": []}, {"lat": 30.2655492,"long": -97.7466255,"score": 90,"name": "W Austin","id": 3,"heatmap": []}]}';
+        heatmap = lodging_result["nearby_noisemakers"]
+        lodging["heatmap"] = heatmap
+        output.append(lodging)
+    return output
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 9099))
     app.run(host='0.0.0.0', port=port)
-    qpd = QuietPlacesData('data_file.pkl', True)
+    qpd = QuietPlacesData('data_file.pkl', False)
 
 
     # tone_analyzer = ToneAnalyzerV3(username='5637355c-a50a-496c-8305-b8820d5c4b63',
